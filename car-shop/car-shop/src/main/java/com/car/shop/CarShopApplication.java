55407549fee6c1477359dc42bebbe10ce20e6151
VasileGaciu <76475116+VasileGaciu@users.noreply.github.com>
2022-07-04 10:34:18 +0300

    Update CarShopApplication.java
    Test

--- car-shop/car-shop/src/main/java/com/car/shop/CarShopApplication.java
+++ car-shop/car-shop/src/main/java/com/car/shop/CarShopApplication.java
@@ -14,6 +14,7 @@ public class CarShopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CarShopApplication.class, args);
 		log.info("--------------Hello world-------------");
+		log.info("--------------Comit----------------------");
 	}
 
 }
